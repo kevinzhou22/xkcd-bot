9c5c15816621370fec047c30b8d5ef6ac4d6ea89
Kevin Zhou <kevinzhou.kz@outlook.com>
2021-05-16 10:09:06 -0400

    Move js files to src

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const client = new Discord.Client();
 client.commands = new Discord.Collection();
 client.cooldowns = new Discord.Collection();
 
-const commandFiles = fs.readdirSync('./commands').filter((file) => file.endsWith('.js'));
+const commandFiles = fs.readdirSync('./src/commands').filter((file) => file.endsWith('.js'));
 
 const commandLoadPromises = commandFiles.map((file) => import(`./commands/${file}`).then((module) => {
   const command = module.default;
